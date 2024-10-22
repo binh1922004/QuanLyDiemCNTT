12839a14355a84648c6ce2ba8d3bd1c2c10084c1
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2024-10-23 01:47:02 +0700

    fix ham xem chi tiet diem

--- QuanLyDiemCNTT/View/MenuChinh.cs
+++ QuanLyDiemCNTT/View/MenuChinh.cs
@@ -29,50 +29,7 @@ namespace QuanLyDiemCNTT.view
             // Gán sự kiện CellClick ở đây
             dgv.CellClick += dgv_CellClick;
         }
-        private void showDiemSinhVien()
-        {
-            string query = "SELECT " +
-                "hp.HocKy, " +
-                "hp.NamHoc, " +
-                "hp.MaHP, " +
-                "hp.TenHP, " +
-                "AVG(bd.DiemQT + bd.DiemKT) / 2 AS DiemTB, " +  // Tính điểm trung bình
-                "dk.MaSV " +  // Thêm MaSV để có thể sử dụng sau này
-                "FROM DangKy dk " +
-                "JOIN HocPhan hp ON dk.MaHP = hp.MaHP " +
-                "JOIN BangDiem bd ON dk.MaHP = bd.MaHP AND dk.MaSV = bd.MaSV " +
-                "WHERE dk.MaSV = 'SV001' " +
-                "GROUP BY hp.HocKy, hp.NamHoc, hp.MaHP, hp.TenHP, dk.MaSV"; // Nhóm theo các cột không tính tổng
-            db.openConnection();
-            SqlCommand cmd = new SqlCommand(query, db.getConnection);
-
-            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
-
-            // Thêm cột "Chi tiết"
-            dt.Columns.Add("Chi tiết", typeof(string));
-            foreach (DataRow row in dt.Rows)
-            {
-                row["Chi tiết"] = "Xem chi tiết";  // Thiết lập văn bản cho cột chi tiết
-            }
-
-            
-
-            dgv.DataSource = dt;
-
-            // Đặt tên cột điểm TB
-            //dgv.Columns["DiemTB"].HeaderText = "Điểm TB";
-
-            // Ẩn các cột điểm QT và điểm KT
-            // (Do chúng ta không lấy cột này trong truy vấn, nên không cần phải ẩn)
-
-            // Thay đổi độ rộng của các cột để dễ nhìn hơn
-            //dgv.Columns["DiemTB"].Width = 100;
-            //dgv.Columns["Chi tiết"].Width = 100;
-
-            db.closeConnection();
-        }
+        
 
         private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
         {
@@ -84,42 +41,9 @@ namespace QuanLyDiemCNTT.view
                 string maSV = dgv.Rows[e.RowIndex].Cells["MaSV"].Value.ToString();
 
                 // Gọi một hàm khác để lấy điểm QT và KT của học phần này
-                ShowDetailedScores(maSV, maHP);
+                sinhVien.ShowDetailedScores(maSV, maHP);
             }
             
-        }
-
-        private void ShowDetailedScores(string maSV, string maHP)
-        {
-            // truy vấn lấy điểm QT, KT và tên học phần
-            string query = "SELECT hp.TenHP, bd.DiemQT, bd.DiemKT " +
-                           "FROM BangDiem bd " +
-                           "JOIN HocPhan hp ON bd.MaHP = hp.MaHP " +
-                           "WHERE bd.MaSV = @MaSV AND bd.MaHP = @MaHP";
-
-            db.openConnection();
-            SqlCommand cmd = new SqlCommand(query, db.getConnection);
-            cmd.Parameters.AddWithValue("@MaSV", maSV);
-            cmd.Parameters.AddWithValue("@MaHP", maHP);
-
-            SqlDataReader reader = cmd.ExecuteReader();
-
-            if (reader.Read())
-            {
-                string tenHP = reader.GetString(0); // Lấy tên học phần
-                double diemQT = reader.GetDouble(1); // Lấy điểm QT
-                double diemKT = reader.GetDouble(2); // Lấy điểm KT
-
-                // Tạo và hiển thị form chi tiết điểm với tên học phần
-                ChiTietDiem formChiTietDiem = new ChiTietDiem(tenHP, diemQT, diemKT);
-                formChiTietDiem.ShowDialog(); // Hiển thị dialog
-            }
-            else
-            {
-                MessageBox.Show("Không tìm thấy điểm chi tiết!", "Thông báo");
-            }
-
-            db.closeConnection();
         }
 
         private void openChildForm(Form childForm)
